9169ec942a92bba5b3c2d92c4c62e2800eeef6e5
kmeraz2 <54649455+kmeraz2@users.noreply.github.com>
2019-09-09 22:53:29 -0700

    Add files via upload

---
+++ dice2.py
@@ -0,0 +1,17 @@
+#I did this with the original program sheet
+from random import choices
+ntrials = 10000
+limit = 6
+player1wins=0
+for i in range(ntrials):
+    rand1 = choices(range(1,limit+1),k=3)
+    rand1.sort(reverse=True)
+    rand2 = choices(range(1,limit+1),k=2)
+    if rand2[0] == rand2[1]:
+        continue
+    if rand2[0]==2 and rand2[1]==2:
+        player1wins=player1wins+1
+        continue
+    if rand1[0]+rand1[1] > rand2[0]+rand2[1]:
+        player1wins=player1wins+1
+print("Fraction=",player1wins/ntrials)
